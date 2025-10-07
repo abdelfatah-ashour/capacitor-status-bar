b86e51514ac78eb0c5a7cfe245d929cddcc13402
Abdelfattah Ashour <abdelfatah.ashour@leadlliaison.com>
2025-10-07 14:22:58 +0300

    Implement StatusBar plugin For IOS and Android.

--- example-app/src/js/example.js
+++
@@ -1,6 +0,0 @@
-import { StatusBar } from 'capacitor-status-bar';
-
-window.testEcho = () => {
-    const inputValue = document.getElementById("echoInput").value;
-    StatusBar.echo({ value: inputValue })
-}
